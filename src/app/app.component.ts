a2fd59a9428419121a2e6f468a1f198632646177
CHAURASIYA MAHADEV <mahadev.chaurasiya@CHAURASIYAs-MacBook-Pro.local>
2023-07-16 22:05:14 +0530

    Ag-grid community version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { CellClickedEvent, ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild(AgGridAngular) agGridApi!: AgGridAngular;
   title = 'ag-grid-app';
+  rowData: any[] = [
+    { Brand: 'Tata', Color: 'White', Model: 'SX', Price: '10L' },
+    { Brand: 'Mahindra', Color: 'White', Model: 'SX', Price: '10L' },
+    { Brand: 'Hyundai', Color: 'White', Model: 'SX', Price: '10L' },
+    { Brand: 'Scoda', Color: 'White', Model: 'SX', Price: '10L' },
+    { Brand: 'Tesla', Color: 'White', Model: 'SX', Price: '10L' },
+    { Brand: 'MG', Color: 'White', Model: 'SX', Price: '10L' },
+    { Brand: 'Maruti', Color: 'White', Model: 'SX', Price: '10L' },
+    { Brand: 'Toyota', Color: 'White', Model: 'SX', Price: '10L' },
+    { Brand: 'Nissan', Color: 'White', Model: 'SX', Price: '10L' }
+  ];
+  colDef: ColDef[] = [
+    { field: 'Brand'},
+    { field: 'Color'},
+    { field: 'Model'},
+    { field: 'Price'}
+  ];
+  defaultAgGridColDef: ColDef = {
+    sortable: true, filter: true
+  }
+
+  onCelClickEvent(event: CellClickedEvent){
+    console.log(event)
+  }
+  clearSelection(){
+    this.agGridApi.api.deselectAll();
+  }
 }
